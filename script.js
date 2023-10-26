924f6d30d3883496a4eb17a76a2226a1973900ab
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-10-26 13:39:56 -0500

    Stuff

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function onReady() {
   console.log("Yay!, onReady() got called when the page loaded.")
+  // Document is how you manipulate the HTML
+  let header = document.querySelector("h1")
+  console.log(header)
+  header.classList.add("pink-text")
+  header.textContent = "HELLO FROM JAVASCRIPT!"
+}
+
+function onPotatoClick() {
+  console.log("You clicked potato!")
+  let potatoFarm = document.getElementById("potatoFarm")
+  potatoFarm.innerHTML += `<p onclick="deletePotato(event)">🥔</p>`
+}
+
+function deletePotato(event) {
+  console.log("you wanna delete a potat", event.target)
+  event.target.remove()
+}
+
+function onUnicornClick() {
+  console.log("Unicorn button clicked")
+  const potatoFarm = document.getElementById("potatoFarm")
+  const unicorn = document.createElement("p")
+  unicorn.innerText = "🦄"
+  unicorn.addEventListener("click", deleteUnicorn)
+  potatoFarm.appendChild(unicorn)
+}
+
+function deleteUnicorn(e) {
+  e.target.remove()
 }
 
 // Will call onReady() when the page loads.
